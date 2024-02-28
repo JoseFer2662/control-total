13507b1fa0851b771df354afba7f45f323d42599
Jose Duenas <joseduego@gmail.com>
2024-02-27 19:58:29 -0500

    Agregamos express

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+const PORT = process.env.PORT
+server.listen(PORT, function(){
+    console.log("Aplicacion corriendo en EL PUERTO: " + PORT)
+})
+
+
+
+/* require('dotenv').config()
 //const dotenv = require('dotenv')
 //dotenv.config()
 const http = require('http')
 
 function requestController(){
     console.log('Hola Mundo!!!!!!!')
+    
 }
 
 const server = http.createServer(requestController)
 const PORT = process.env.PORT
 server.listen(PORT, function(){
     console.log("Aplicacion corriendo en EL PUERTO: " + PORT)
-})
+}) */
+
